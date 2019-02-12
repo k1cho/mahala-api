c3f749b27951d5d97757059bf29fdf774450250a
k1cho <aadnanhadzipasic@gmail.com>
2019-02-12 19:24:53 +0100

    auth register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/user')
 const Helpers = require('../helpers/helpers')
+const bcrypt = require('bcryptjs')
 
 exports.register = async (req, res) => {
     const email = await User.findOne({
@@ -21,4 +22,23 @@ exports.register = async (req, res) => {
             message: 'Username already exists.'
         })
     }
+
+    bcrypt.hash(req.body.password, 10).then((result) => {
+        const user = new User({
+            username: Helpers.firstUppercase(req.body.username),
+            email: Helpers.lowerCase(req.body.email),
+            password: result
+        })
+
+        user.save((err) => {
+            if (err) {
+                return res.status(422).send({
+                    message: err.message
+                })
+            }
+            return res.status(201).json({
+                message: 'Successfully registered.'
+            })
+        })
+    })
 }
