61535b9717b9d302eeda99c36d6f678714f5612f
k1cho <aadnanhadzipasic@gmail.com>
2019-02-12 13:40:20 +0100

    server config

--- server.js
+++ server.js
@@ -2,3 +2,19 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb+srv://bkichob:ZzK6J1LfqjbApbeP@cluster0-r1ldt.mongodb.net/test?retryWrites=true', {
+        useNewUrlParser: true
+    })
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch(() => {
+        console.log('Connection error');
+    })
+
+
+app.listen(3000, () => {
+    console.log('Running on port 3000')
+})
